3319a1d4b84e398f11b1023d5c8106e8d8ee8937
github-aliceyang <aliyang47@gmail.com>
2022-08-04 10:48:52 -0700

    added weather forecast in HTML and javascript function "displayForecast"
    1. removed HTML file, empty div.
    2. created new function "displayForecast" in Javascript.
    3. selected the elements and created a new string "forecastHTML" for class and string.
    4. created a new variable for the days. (array)
    5. used forEach function to loop through the days.

--- src/app.js
+++ src/app.js
@@ -14,6 +14,31 @@ function formatDate(timestamp) {
     return ` ${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row" >`;
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+    days.forEach(function (day) {
+        forecastHTML = forecastHTML + `
+        <div class="col-2">
+            <div class="weather-forecast-date">
+               ${day}
+              </div>
+                <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" width="42px"
+                />
+                <div class ="weather-forecast-temperature">
+                <span class = "weather-forecast-temperature-max"> 20° </span>
+                <span class = "weather-forecast-temperature-min"> 18° </span>
+            </div>
+        </div>
+    `;
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -67,6 +92,8 @@ function displayCelsiusTemp(event) {
 
 let celsiusTemperature = null;
 
+
+
 let form = document.querySelector(`#search-form`);
 form.addEventListener("submit", handleSubmit);
 
@@ -77,4 +104,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search(`New York`);
+displayForecast();
 
