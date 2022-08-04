58b0306bea067f39940fd8bc7aa400712af417c6
github-aliceyang <aliyang47@gmail.com>
2022-08-04 14:53:23 -0700

    display the forecast data using the API
    1. stored the response from the API inside the variable called "forecast".
    2. instead of looping thru the days, we are looping thru the "forecast" variable which is an array of each coming day.
    3. then looped thru an object and getting the index of current item in the array, then format the day stamp.
    4. included the icon name inside the image url and rounded the max/min temperatures.
    5. lastly, checked the index to display the first 6 days instead of 8 days.

--- src/app.js
+++ src/app.js
@@ -14,26 +14,40 @@ function formatDate(timestamp) {
     return ` ${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
 
+
+
     let forecastHTML = `<div class="row" >`;
-    let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
-    days.forEach(function (day) {
-        forecastHTML = forecastHTML + `
+    //let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+            forecastHTML = forecastHTML +
+                `
         <div class="col-2">
             <div class="weather-forecast-date">
-               ${day}
+               ${formatDay(forecastDay.dt)}
               </div>
-                <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="icon" width="42px"
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"
                 />
                 <div class ="weather-forecast-temperature">
-                <span class = "weather-forecast-temperature-max"> 20° </span>
-                <span class = "weather-forecast-temperature-min"> 18° </span>
+                <span class = "weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+                <span class = "weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
             </div>
         </div>
     `;
+        }
     });
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
@@ -117,5 +131,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search(`New York`);
-displayForecast();
 
