ad18ad0de39b83b584f59eea314ba0eed5e212a4
github-aliceyang <aliyang47@gmail.com>
2022-08-05 10:21:51 -0700

    added background color in CSS

--- src/app.js
+++ src/app.js
@@ -27,8 +27,6 @@ function displayForecast(response) {
 
     let forecastElement = document.querySelector("#forecast");
 
-
-
     let forecastHTML = `<div class="row" >`;
     //let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
     forecast.forEach(function (forecastDay, index) {
@@ -54,10 +52,10 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-    console.log(coordinates);
+    //console.log(coordinates);
     let apiKey = `41653189222a9f622e0f370d2ef32efe`;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=41653189222a9f622e0f370d2ef32efe&units=metric`;
-    console.log(apiUrl);
+    //console.log(apiUrl);
     axios.get(apiUrl).then(displayForecast);
 }
 
@@ -69,6 +67,8 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let maxTemp = document.querySelector("#high");
+    let minTemp = document.querySelector("#low");
     let iconElement = document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
     celsiusTemperature = response.data.main.temp;
@@ -78,6 +78,8 @@ function displayTemperature(response) {
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    maxTemp.innerHTML = Math.round(response.data.main.temp_max);
+    minTemp.innerHTML = Math.round(response.data.main.temp_min);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
 
@@ -96,6 +98,14 @@ function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector(`#city-input`);
     search(cityInputElement.value);
+}
+
+function showPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiKey = `41653189222a9f622e0f370d2ef32efe`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=41653189222a9f622e0f370d2ef32efe&units=${units}`;
+    axios.get(apiUrl).then(displayTemperature);
 }
 
 
@@ -119,8 +129,6 @@ function displayCelsiusTemp(event) {
 
 let celsiusTemperature = null;
 
-
-
 let form = document.querySelector(`#search-form`);
 form.addEventListener("submit", handleSubmit);
 
@@ -130,5 +138,17 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
-search(`New York`);
+search(`San Jose`);
 
+
+navigator.geolocation.getCurrentPosition(showPosition);
+console.log(showPosition);
+
+
+function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentLocation = document.querySelector("#current-location-btn")
+currentLocation.addEventListener(`click`, getCurrentLocation);
